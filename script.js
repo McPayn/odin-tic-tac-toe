ae02785f0a8a8b4c931bedd0d0dc70d975b214b0
Tyler <brown.tyler.w@gmail.com>
2023-10-11 09:52:55 -0700

    Added some functionality change Can click on divs to change marker, buggy

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
-const gameboard = [];
+const marker = 0;
+function Gameboard () {
+    const board = [];
+    const getGameboard = (i) => board[i];
+    const setGameboard = (stuff, i) => board[i] = stuff;
+    return {getGameboard, setGameboard};
+}
 
+const gameboard = new Gameboard;
 for (let i = 0; i < 9; i++) {
     if (i % 2 == 0) {
-        gameboard[i] = 'X';
+        gameboard.setGameboard('X', i);
     } else {
-        gameboard[i] = 'O';
+        gameboard.setGameboard('O', i);
     }
 }
 
+function changeMarker(mark) {
+    marker = mark;
+}
+
+function changeBoard(e) {
+    gameboard.setGameboard(marker, e.target.id);
+}
 function fillBoard() {
     const container = document.querySelector('#container');
+    container.innerHTML = '';
     for (let i = 0; i < 9; i++) {
         const new_grid = document.createElement('div');
         new_grid.classList.add('board-grid');
-        new_grid.innerHTML = gameboard[i];
+        new_grid.setAttribute('id', i);
+        new_grid.onclick = function (e) {
+            gameboard.setGameboard(marker, e.target.id);
+            fillBoard();
+        }
+        new_grid.innerHTML = gameboard.getGameboard(i);
         container.append(new_grid);
     }
 }
