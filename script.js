35f1cfdbedc8ec632894483f603ed5d8f7181fd9
Tyler Brown <brown.tyler.w@gmail.com>
2023-10-11 19:20:24 -0700

    Some refactoring + functionality added Game now switches between X and O with every input, marker selection button works as intended Added reset button to clear board, and added indicator to show player's turn

--- script.js
+++ script.js
@@ -1,47 +1,64 @@
 function Gameboard () {
-    const board = [];
+    const board = Array(9).fill('');
+    let playerTurn = true;
     const getGameboard = (i) => board[i];
     const setGameboard = (stuff, i) => board[i] = stuff;
-    return {getGameboard, setGameboard};
+    const getPlayerTurn = () => playerTurn;
+    const setPlayerTurn = (val) => playerTurn = val;
+    const clearGameboard = () => {
+        for (let j = 0; j < 9; j++) {
+            board[j] = '';
+        }
+    };
+    return {getGameboard, setGameboard, clearGameboard, getPlayerTurn, setPlayerTurn};
 }
 
 function createPlayer(mark) {
     let marker = mark;
     const getMarker = () => marker;
-    const setMarker = (new_mark) => marker=new_mark;
+    const setMarker = (new_mark) => marker = new_mark;
     return {setMarker, getMarker};
 }
 
 const gameboard = new Gameboard;
-const player = createPlayer("X");
-
-for (let i = 0; i < 9; i++) {
-    if (i % 2 == 0) {
-        gameboard.setGameboard('X', i);
-    } else {
-        gameboard.setGameboard('O', i);
-    }
-}
+const player1 = createPlayer("X");
+const player2 = createPlayer("O");
 
 function changeMarker(mark) {
-    player.setMarker(mark);
+    let mark_2 = '';
+    if (mark == 'X') { mark_2 = 'O'; }
+    else { mark_2 = 'X'; }
+    player1.setMarker(mark);
+    player2.setMarker(mark_2);
 }
 
 function changeBoard(e) {
     gameboard.setGameboard(marker, e.target.id);
 }
+
 function fillBoard() {
-    const container = document.querySelector('#container');
-    container.innerHTML = '';
     for (let i = 0; i < 9; i++) {
-        const new_grid = document.createElement('div');
-        new_grid.classList.add('board-grid');
-        new_grid.setAttribute('id', i);
-        new_grid.onclick = function (e) {
-            gameboard.setGameboard(player.getMarker(), e.target.id);
-            fillBoard();
-        }
+        const new_grid = document.getElementById(i);
         new_grid.innerHTML = gameboard.getGameboard(i);
-        container.append(new_grid);
     }
+    const player_turn = document.querySelector('.turn-notif');
+    const play = gameboard.getPlayerTurn() ? '1' : '2';
+    player_turn.innerHTML = 'Player ' + play + ' turn';
+}
+
+function setGrid(id) {
+    if (gameboard.getGameboard(id) == '') {
+        const turn = gameboard.getPlayerTurn();
+        const player = turn ? player1 : player2;
+        gameboard.setGameboard(player.getMarker(), id);
+        fillBoard();
+        gameboard.setPlayerTurn(!turn);
+    }
+}
+
+function resetBoard() {
+    for (let i = 0; i < 9; i++) {
+        gameboard.setGameboard('', i);
+    }
+    fillBoard();
 }
